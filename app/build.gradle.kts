a315c42ee9a63a3b0f14ffc51b8dd183400de278
Peñafiel del Rosario <gabriel.penafieldel@ug.edu.ec>
2025-05-13 19:05:28 -0500

    Actualizacion mas reciente de firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     //id("com.android.application")
-    id("com.google.gms.google-services") // <-- ESTA LÍNEA
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -39,9 +39,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
-    implementation("com.google.firebase:firebase-auth:22.3.0")
-    implementation("com.google.firebase:firebase-database:20.3.0")
-    implementation("com.google.firebase:firebase-storage:20.3.0")
+    implementation(platform("com.google.firebase:firebase-bom:33.13.0"))
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
